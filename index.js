8a78076b999b4206ac54ad44c136af757ea771f1
Ethan Isola <isolaethan@gmail.com>
2022-01-26 17:38:32 -0600

    commit tree a93e07e830ad4c1fb26c16e8dfa3615b256e2d42

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let checker = false
+  for (let i = 0; i < array.length; i++) {
+    // look through the rest of the pile
+    for (let j = i + 1; j < array.length; j++) {
+      // check if it matches the first sock
+      if (array[i] + array[j] == target) {
+        checker = true;
+      }
+    }
+  }
+  return checker
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  take array and target number
+  loop take each number
+  add it to every other number
+  if they add up to 25 return true
+  otherwise return false by default
 */
 
 /*
   Add written explanation of your solution here
+  I made a variable thats set to false by default. then I made two loops
+  that takes a number, then takes another number and adds them together.
+  If the sum is equal to the target number, the variable becomes true, otherwise
+  it remains unchanged.
 */
 
 // You can run `node index.js` to view these console logs
